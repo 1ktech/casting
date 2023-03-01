ad34d052c2d080bb5ad18f635ed20f1e20c6fe11
Akash Rawat <akash.rawat@niyotail.com>
2023-03-01 15:20:09 +0530

    casting

---
+++ chumbak.js
@@ -0,0 +1,13 @@
+const getTemplate = ()=>{
+ url= 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+ return url;
+}
+
+const watchTime = ()=>{
+
+}
+
+export {
+    getTemplate,
+    watchTime
+}                               
